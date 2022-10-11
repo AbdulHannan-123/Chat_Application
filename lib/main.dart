dd244cb1f69e7253d312224e89a5d83faf293e27
AbdulHannan-123 <ah6475774@gmail.com>
2022-10-11 17:20:49 +0500

    Initializing  the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:chat_application_2022/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    // ignore: prefer_const_constructors
+    options: FirebaseOptions(
+        apiKey: "AIzaSyDUEjdwhqEOpZXaro7s5Z5rKCnaZS7ZxW0",
+        appId: "",
+        messagingSenderId: "",
+        projectId: "flutter-chat-e368c"),
+  );
   runApp(MyApp());
 }
 
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,16 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Container(
-          padding:const EdgeInsets.all(8),
-          child:const Text('This works!'),
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) => Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text('This works!'),
+          ),
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection('chats/wLKN067msappUusCZc9f/messages')
+                .snapshots()
+                .listen((event) {
+              event.docs.forEach((element) {
+                print(element['text']);
+              });
+            });
+          },
+          child: const Icon(Icons.add),
+        ));
   }
 }
