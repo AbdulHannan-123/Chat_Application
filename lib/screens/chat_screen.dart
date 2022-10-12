6bf132ba59db5cd66fd5b183e89c1d1ce2fee068
AbdulHannan-123 <ah6475774@gmail.com>
2022-10-12 14:46:41 +0500

    Adding adn fatching Data from firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -5,22 +7,30 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) => Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('This works!'),
-          ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('chats/wLKN067msappUusCZc9f/messages')
+              .snapshots(),
+          builder: (ctx, AsyncSnapshot streamSnapshot) {
+            if (streamSnapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final documents = streamSnapshot.data!.docs;
+            return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (ctx, index) => Container(
+                padding: const EdgeInsets.all(8),
+                child:  Text(documents[index]['text']),
+              ),
+            );
+          },
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            FirebaseFirestore.instance
-                .collection('chats/wLKN067msappUusCZc9f/messages')
-                .snapshots()
-                .listen((event) {
-              event.docs.forEach((element) {
-                print(element['text']);
-              });
+            FirebaseFirestore.instance.collection('chats/wLKN067msappUusCZc9f/messages').add({
+              'text': 'this was added by clicking the button',
             });
           },
           child: const Icon(Icons.add),
