b52568f60f87a365aa4e07a82a6aca6228b36c2f
AbdulHannan-123 <ah6475774@gmail.com>
2022-10-15 00:52:38 +0500

    validate login page

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -8,6 +8,20 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  String _userName = '';
+  String _userEmail = '';
+  String _userPass = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+    if (isValid) {
+      _formKey.currentState!.save();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -18,37 +32,76 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('username'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "You Cant Leave Username Empty";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(labelText: 'Username'),
+                      onSaved: (newValue) {
+                        _userName = newValue!;
+                      },
+                    ),
                   TextFormField(
+                    key: ValueKey('email'),
+                    validator: (value) {
+                      if (value!.isEmpty || value.contains('@')) {
+                        return 'Enter a valid email';
+                      }
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: const InputDecoration(
                       labelText: 'EmailAddress',
                     ),
+                    onSaved: (newValue) {
+                      _userEmail = newValue!;
+                    },
                   ),
                   TextFormField(
-                      decoration: const InputDecoration(labelText: 'Username')),
-                  TextFormField(
+                    key: ValueKey('password'),
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 7) {
+                        return "Pasword must contain 7 charator";
+                      }
+                      return null;
+                    },
                     decoration: const InputDecoration(
                       labelText: 'Password',
                     ),
                     obscureText: true,
+                    onSaved: (newValue) {
+                      _userPass = newValue!;
+                    },
                   ),
                   const SizedBox(
                     height: 10,
                   ),
                   RaisedButton(
-                    child: const Text('Login'),
-                    onPressed: () {},
+                    onPressed: _trySubmit,
+                    child: Text(_isLogin ? 'Login' : 'Signup'),
                   ),
                   const SizedBox(height: 4),
                   OutlinedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
                     style: OutlinedButton.styleFrom(
-                      side: BorderSide(color: Theme.of(context).colorScheme.primary)
-                    ) ,
-                    child: const Text('Create New Account'),
+                        side: BorderSide(
+                            color: Theme.of(context).colorScheme.primary)),
+                    child: Text(_isLogin
+                        ? 'Create New Account'
+                        : 'I already have an account'),
                   )
                 ],
               ),
